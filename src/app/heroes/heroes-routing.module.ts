f1bf3ed41f196f8f3c5b4abce2f03353c055faa7
ruminahuelwalter <ruminahuelwalter@gmail.com>
2024-08-31 17:31:47 -0300

    rutas hijas y lazyLoad

--- src/app/heroes/heroes-routing.module.ts
+++ src/app/heroes/heroes-routing.module.ts
@@ -1,12 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { LayoutPageComponent } from './pages/layout-page/layout-page.component';
+import { NewPageComponent } from './pages/new-page/new-page.component';
 
 //localhost:4200/heroes/
 const routes: Routes = [
   {
     path: '',
-    component: LayoutPageComponent
+    component: LayoutPageComponent,
+    children: [
+      {
+        path: 'new-hero', component: NewPageComponent
+      },
+    ]
   }
 ];
 
